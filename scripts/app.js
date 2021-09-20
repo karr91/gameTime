ae98748ddf290d0d73b2fcbd64801c8aef287d2f
Michael Karr <michaelhkarr@gmail.com>
2021-09-20 17:27:53 -0500

    added functionality to the canvas with a ball that will bounce

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,37 @@
+// ball on canvas
 
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+
+var dx = 2;
+var dy = -2;
+
+var ballRadius = 10
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+}
+function ball() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+
+    x += dx;
+    y += dy;
+}
+
+
+setInterval(ball, 10);
